55848ecf7b874842c7f3a4e204165e7e1a028ff3
vshhesyuk <vshhesyuk@mail.ru>
2019-07-27 16:53:25 +0700

    Решение задачи 0 и 1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -5,5 +5,19 @@
  * @returns {number}
  */
 function pow(m, n) {
+  let result = 1;
+    for (let i = 0; i < n; i++) {
+      result *= x;
+    }
 
+    return result;
 }
+
+let x = +prompt("Enter: x", '0');
+let n = +prompt("Enter: n", "0");
+
+  if (n < 1 ) {
+    n = +prompt("Enter: n > 1", "0");
+  } else {
+    alert( pow(x, n) );
+  }
