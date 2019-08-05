7e9ad0e3ddb08a53696d949556bf8084f81202f1
vshhesyuk <vshhesyuk@mail.ru>
2019-08-05 17:53:14 +0700

    Модуль 2 Задача: Клонирование объектов

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,5 +3,16 @@
  * @param {Object} obj - клонируем объект
  * @returns {Object}
  */
+
 function clone(obj) {
+  let clon = {};
+
+    for (let key in obj) {
+      if (typeof obj[key] === 'object' && obj[key] != null) {
+        clon[key] = clone(obj[key]);
+      } else {
+        clon[key] = obj[key];
+      }
+    }
+    return clon;
 }
