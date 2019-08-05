3a20ab5931493ee12f38d409d6fc5caf9d79c3eb
vshhesyuk <vshhesyuk@mail.ru>
2019-08-05 18:52:43 +0700

    Модуль 2 Задача: Пуст ли объект

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -4,4 +4,15 @@
  * @returns {Boolean}
  */
 function isEmpty(obj) {
+  let counter = 0;
+
+  for (let key in obj) {
+    counter++;
+  }
+
+  if (counter === 0) {
+    return true;
+  }
+
+  return false;
 }
